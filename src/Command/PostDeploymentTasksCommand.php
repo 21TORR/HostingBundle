962e20478f98b2ec7124a1f7ad9a8d57920cd8df
Jannik Zschiesche <j.zschiesche@21torr.com>
2023-07-05 10:41:10 +0200

    Fix deprecation in console commands

--- src/Command/PostDeploymentTasksCommand.php
+++ src/Command/PostDeploymentTasksCommand.php
@@ -2,15 +2,16 @@
 
 namespace Torr\Hosting\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Torr\Hosting\Deployment\TaskCli;
 use Torr\Hosting\Deployment\TaskRunners;
 
+#[AsCommand("hosting:post-deploy")]
 final class PostDeploymentTasksCommand extends Command
 {
-	protected static $defaultName = "hosting:post-deploy";
 	private TaskRunners $runners;
 
 	/**
