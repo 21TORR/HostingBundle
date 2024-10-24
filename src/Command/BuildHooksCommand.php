09a2c9bb14b59a1f4b9900be4a38a215220cbe1c
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-10-24 08:56:18 +0200

    Remove deprecated things

--- src/Command/BuildHooksCommand.php
+++ src/Command/BuildHooksCommand.php
@@ -12,7 +12,6 @@ use Torr\Hosting\Deployment\TaskCli;
 #[AsCommand(
 	"hosting:hook:build",
 	description: "Runs the hooks for 'after the build finished'",
-	aliases: ["hosting:run-tasks:post-build"],
 )]
 final class BuildHooksCommand extends Command
 {
@@ -33,21 +32,6 @@ final class BuildHooksCommand extends Command
 	{
 		$io = new TaskCli($input, $output);
 		$io->title("Run Build Hooks");
-
-		// @todo remove code block + alias in v4
-		if ("hosting:hook:build" !== $input->getFirstArgument())
-		{
-			$message = \sprintf(
-				"The command `%s` was deprecated. Use `hosting:hook:build` instead.",
-				$input->getFirstArgument(),
-			);
-			trigger_deprecation(
-				"21torr/hosting",
-				"3.1.0",
-				$message,
-			);
-			$io->caution($message);
-		}
 
 		$this->runners->runBuildHooks($io);
 
