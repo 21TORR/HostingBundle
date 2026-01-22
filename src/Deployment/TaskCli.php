f4bece78f3bc3f0640d822935e5c2da8295b2edb
Jannik Zschiesche <j.zschiesche@21torr.com>
2026-01-22 18:35:07 +0100

    Bump cli

--- src/Deployment/TaskCli.php
+++ src/Deployment/TaskCli.php
@@ -6,14 +6,4 @@ use Torr\Cli\Console\Style\TorrStyle;
 
 final class TaskCli extends TorrStyle
 {
-	/**
-	 *
-	 */
-	public function done (string $message) : void
-	{
-		$this->write(\sprintf(
-			"<fg=green>✓</> %s",
-			$message,
-		));
-	}
 }
