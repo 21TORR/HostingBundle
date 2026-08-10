c961bf0fce264859e4081c03adbd2e709eda863f
Jannik Zschiesche <j.zschiesche@21torr.com>
2026-08-10 10:30:06 +0200

    Add init hooks

--- src/Deployment/HookRunners.php
+++ src/Deployment/HookRunners.php
@@ -8,6 +8,7 @@ final readonly class HookRunners
 {
 	public const string TAG_BUILD_HOOK = "hosting.hook.build";
 	public const string TAG_DEPLOY_HOOK = "hosting.hook.deploy";
+	public const string TAG_INIT_HOOK = "hosting.hook.init";
 
 	/**
 	 */
@@ -18,6 +19,9 @@ final readonly class HookRunners
 		/** @var DeployHookInterface[] */
 		#[AutowireIterator(tag: self::TAG_DEPLOY_HOOK)]
 		private iterable $deployHooks,
+		/** @var InitHookInterface[] */
+		#[AutowireIterator(tag: self::TAG_INIT_HOOK)]
+		private iterable $initHooks,
 	) {}
 
 	/**
@@ -61,6 +65,28 @@ final readonly class HookRunners
 
 			$io->section("Run Deploy Hook: <fg=magenta>{$runner->getLabel()}</>");
 			$runner->runPostDeployment($io);
+		}
+	}
+
+	/**
+	 */
+	public function runInitHooks (TaskCli $io) : void
+	{
+		$first = true;
+
+		foreach ($this->initHooks as $runner)
+		{
+			if ($first)
+			{
+				$first = false;
+			}
+			else
+			{
+				$io->newLine(2);
+			}
+
+			$io->section("Run Init Hook: <fg=magenta>{$runner->getLabel()}</>");
+			$runner->runPostInit($io);
 		}
 	}
 }
---
+++ src/Deployment/InitHookInterface.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Torr\Hosting\Deployment;
+
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag(HookRunners::TAG_INIT_HOOK)]
+interface InitHookInterface
+{
+	/**
+	 * Returns the label of the hook
+	 */
+	public function getLabel () : string;
+
+	/**
+	 * Runs the post initialization hook
+	 */
+	public function runPostInit (TaskCli $io) : void;
+}
